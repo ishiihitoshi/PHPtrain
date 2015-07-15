5b7fe721c2c1b58e3aa3dbb9613df169ce6de09b
石井 均 <ishii@cup.com>
2015-07-15 14:01:24 +0900

    初めてのコミット

---
+++ thanks.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>ひと言掲示板</title>
+</head>
+
+<body>
+<p>ユーザー登録が完了しました</p>
+<p><a href="../">ログインする</a></p>
+
+</body>
+</html>
