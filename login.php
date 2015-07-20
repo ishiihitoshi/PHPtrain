90aa7f910bfa93a538e623566173ee3465fe6e96
石井 均 <ishii@cup.com>
2015-07-20 21:49:03 +0900

    ログインの仕組みを追加

---
+++ login.php
@@ -0,0 +1,62 @@
+<?php
+require('dbconnect.php');
+
+session_start();
+
+if(!empty($_POST)){
+	//ログインの処理
+	if($_POST['email'] !='' && $_POST['password']!=''){
+		$sql = sprintf('SELECT * FROM members WHERE email="%s' AND password="%s",
+			mysqli_real_escape_strings($db, $_POST['email']),
+			mysqli_real_escape_strings($db, sha1($_POST['password']))
+			);
+		$record = mysqli_query($db, $sql) or die(mysqli_error($db));
+		if($table=mysqli_fetch_assoc($record)){
+			//ログイン成功
+			$_SESSION['id'] = $table['id'];
+			$_SESSION['time'] = time();
+			header('Location: index.php');
+			exit();
+		}else{
+			$error['login'] = 'failed';
+		}
+	}else{
+		$error['login']='blank';
+	}
+}
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>ひと言掲示板</title>
+</head>
+
+<body>
+<div id="lead">
+<p>メールアドレスとパスワードを入力してログインしてください</p>
+<p>入会手続きがまだの方はこちらからどうぞ</p>
+<p>&raquo;<a href="join/">入会手続きをする</a></p>
+</div>
+
+<form action="" method="post">
+  <dl>
+  	<dt>メールアドレス</dt>
+  	<dd>
+  		<input type="text" name="email" size="35" maxlength="255" />
+  	</dd>
+  	<dt>パスワード</dt>
+  	<dd>
+  		<input type="password" name="password" size="35" maxlength="255" />
+  	</dd>
+  	<dt>ログイン情報の記録</dt>
+  	<dd>
+  		<input id="save" type="checkbox" name="save" value="on"><label for="save">次回からは自動的にログインする</label>
+  	</dd>
+  </dl>
+  <div><input tye="submit" value="ログインする" /></div>
+</form>
+
+</body>
+</html>
