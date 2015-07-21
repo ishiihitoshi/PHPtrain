cd117298ab05ebe03272299dbce8ae6332f3bbf5
石井 均 <ishii@cup.com>
2015-07-21 22:27:13 +0900

    loginセッションを記入

--- login.php
+++ login.php
@@ -44,11 +44,19 @@ if(!empty($_POST)){
   <dl>
   	<dt>メールアドレス</dt>
   	<dd>
-  		<input type="text" name="email" size="35" maxlength="255" />
+  		<input type="text" name="email" size="35" maxlength="255"
+  		value="<?php echo htmlspecialchars($_POST['email']); ?>" />
+  		<?php if($error['login'] == 'blank'): ?>
+  		<p class="error">*メールアドレスとパスワードをご記入ください</p>
+  		<?php endif; ?>
+  		<?php if($error['login'] == 'failed'): ?>
+  		<p class="error">* ログインに失敗しました。正しくご記入ください。</p>
+  		<?php endif; ?>
   	</dd>
   	<dt>パスワード</dt>
   	<dd>
-  		<input type="password" name="password" size="35" maxlength="255" />
+  		<input type="password" name="password" size="35" maxlength="255"
+  		value="<?php echo htmlspecialchars($_POST['password']); ?>" />
   	</dd>
   	<dt>ログイン情報の記録</dt>
   	<dd>
